52e10a4a6e395803fa0eba96c36298273e6b0387
Phodal HUANG <h@phodal.com>
2019-01-22 20:55:33 +0800

    poc: add editor

--- poc/src/app.js
+++ poc/src/app.js
@@ -1,14 +1,26 @@
-dragula([document.getElementById('left-events'), document.getElementById('right-events')], {
-  direction: 'horizontal',
-  moves: function (el, source, handle, sibling) {
-    return true;
-  },
-}).on('drag', function (el) {
-  el.className = el.className.replace('ex-moved', '');
-}).on('drop', function (el) {
-  el.className += ' ex-moved';
-}).on('over', function (el, container) {
-  container.className += ' ex-over';
-}).on('out', function (el, container) {
-  container.className = container.className.replace('ex-over', '');
-});
+function init() {
+  dragula([
+    document.getElementById('zone1'),
+    document.getElementById('zone2'),
+    document.getElementById('zone3')
+  ], {
+    direction: 'horizontal',
+    moves: function (el, source, handle, sibling) {
+      return true;
+    },
+  }).on('drag', function (el) {
+    el.className = el.className.replace('ex-moved', '');
+  }).on('drop', function (el) {
+    el.className += ' ex-moved';
+  }).on('over', function (el, container) {
+    container.className += ' ex-over';
+  }).on('out', function (el, container) {
+    container.className = container.className.replace('ex-over', '');
+  });
+
+
+  var elements = document.querySelectorAll('.editable'),
+    editor = new MediumEditor(elements);
+}
+
+init();

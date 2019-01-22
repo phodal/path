31aa1a50f04cceecbd9cb2590153c36a94c791b0
Phodal HUANG <h@phodal.com>
2019-01-22 20:41:14 +0800

    feat: add basic format

--- poc/src/app.js
+++ poc/src/app.js
@@ -1,7 +1,11 @@
-dragula([document.getElementById('left-events'), document.getElementById('right-events')])
-  .on('drag', function (el) {
-    el.className = el.className.replace('ex-moved', '');
-  }).on('drop', function (el) {
+dragula([document.getElementById('left-events'), document.getElementById('right-events')], {
+  direction: 'horizontal',
+  moves: function (el, source, handle, sibling) {
+    return true;
+  },
+}).on('drag', function (el) {
+  el.className = el.className.replace('ex-moved', '');
+}).on('drop', function (el) {
   el.className += ' ex-moved';
 }).on('over', function (el, container) {
   container.className += ' ex-over';

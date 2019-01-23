8c09713c17f0aed245ea41f7145b02b71f7fb8c6
Phodal HUANG <h@phodal.com>
2019-01-23 08:49:40 +0800

    feat: set text colors

--- poc/src/app.js
+++ poc/src/app.js
@@ -32,14 +32,18 @@ function initElements() {
       title: 'Process',
       items: [
         '提交代码', 'PUSH Hooks', '运动持续集成', '部署到 Dev 环境', 'E2E 测试', '手动测试', '部署到 UAT 环境', '手工测试', '上线申请', '上线'
-      ]
+      ],
+      backgroundColor: '#99b433',
+      textColor: '#ffffff'
     },
     {
       id: 2,
       title: 'Tools',
       items: [
         'Git & GitHub', 'Git', 'Jenkins', '', '', '', '', '', '', '', '', ''
-      ]
+      ],
+      backgroundColor: '#00a300',
+      textColor: '#ffffff'
     }
   ];
 
@@ -47,7 +51,7 @@ function initElements() {
   for (var i = 0; i < pipeData.length; i++) {
     var id = generateId(pipeData[i].title);
     var childItemId = id + '_child';
-    var currentHtml = "<div id='" + id + "' class='container'>";
+    var currentHtml = `<div id='${id}' class='container' style='background:${pipeData[i].backgroundColor};color:${pipeData[i].textColor}'>`;
 
     for (var k = 0; k < pipeData[i].items.length; k++) {
       currentHtml = currentHtml + '<div class="editable" id="' + childItemId + '">' + pipeData[i].items[k] + '</div>';

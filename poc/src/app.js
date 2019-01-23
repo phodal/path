b551d24b50dccf39ec28d51520178f43fbd90991
Phodal HUANG <h@phodal.com>
2019-01-23 09:40:23 +0800

    feat: add some metro colors

--- poc/src/app.js
+++ poc/src/app.js
@@ -24,6 +24,15 @@ function generateId(pipeData) {
   return pipeData.toLocaleLowerCase();
 }
 
+function changeItemHeight(maxLength) {
+  var items = document.getElementsByClassName('editable');
+
+  for (var i = 0, len = items.length; i < len; i++) {
+    items[i].style["height"] = '150px';
+    items[i].style["width"] = '' + 100 / maxLength + '%';
+  }
+}
+
 function initElements() {
   var ids = [];
   var pipeData = [
@@ -47,20 +56,28 @@ function initElements() {
     }
   ];
 
+  var maxLength = pipeData[0].items.length;
   var elements = '';
   for (var i = 0; i < pipeData.length; i++) {
     var id = generateId(pipeData[i].title);
     var childItemId = id + '_child';
     var currentHtml = `<div id='${id}' class='container' style='background:${pipeData[i].backgroundColor};color:${pipeData[i].textColor}'>`;
 
-    for (var k = 0; k < pipeData[i].items.length; k++) {
+    var itemLength = pipeData[i].items.length;
+    for (var k = 0; k < itemLength; k++) {
       currentHtml = currentHtml + '<div class="editable" id="' + childItemId + '">' + pipeData[i].items[k] + '</div>';
     }
     currentHtml = currentHtml + '</div>';
     elements = elements + currentHtml;
+
+    if (itemLength > maxLength) {
+      maxLength = itemLength;
+    }
   }
 
   document.getElementById('pipe').innerHTML = elements;
+
+  changeItemHeight(maxLength);
 
   for (var j = 0; j < pipeData.length; j++) {
     ids.push(document.getElementById(generateId(pipeData[j].title)));

16b0da8549359739d3953e70ce7bdbd944cb00a7
Phodal HUANG <h@phodal.com>
2019-01-23 20:14:30 +0800

    feat: storage last items

--- poc/src/app.js
+++ poc/src/app.js
@@ -1,3 +1,61 @@
+var pipeData = [
+  {
+    id: 1,
+    title: 'Process',
+    items: [
+      '提交代码', 'PUSH Hooks', '运行持续集成', '部署到 Dev 环境', 'E2E 测试', '手动测试', '部署到 UAT 环境', '手工测试', '上线申请', '上线'
+    ],
+    backgroundColor: '#00a300',
+    textColor: '#ffffff'
+  },
+  {
+    id: 2,
+    title: 'People',
+    items: [
+      ''
+    ],
+    backgroundColor: '#ff0097',
+    textColor: '#ffffff'
+  },
+  {
+    id: 3,
+    title: 'Tooling',
+    items: [
+      'Git & GitHub', 'Git', 'Jenkins', '', '', '', '', '', '', '', '', ''
+    ],
+    backgroundColor: '#99b433',
+    textColor: '#ffffff'
+  },
+  {
+    id: 4,
+    title: 'Artifacts',
+    items: [
+      'Code', '', 'Build Log', '', '', '', '', '', '', '', '', ''
+    ],
+    backgroundColor: '#1e7145',
+    textColor: '#ffffff'
+  }
+];
+
+window.pipeData = pipeData;
+
+function updateContainerPipeData(source) {
+  var sourceId = source.id;
+  var element = document.getElementById(sourceId);
+  var newOrders = [];
+  for (var i = 0; i < element.childNodes.length; i++) {
+    let innerText = element.childNodes[i].innerText;
+    newOrders.push([innerText]);
+  }
+  window.pipeData.filter(item => {
+    if (sourceId === generateId(item.id)) {
+      item.items = newOrders;
+    }
+    return item;
+  });
+  localStorage.setItem('ptop.pipe', JSON.stringify(window.pipeData));
+}
+
 function initPipes(ids) {
   dragula(ids, {
     direction: 'horizontal',
@@ -6,7 +64,10 @@ function initPipes(ids) {
     },
   }).on('drag', function (el) {
     el.className = el.className.replace('ex-moved', '');
-  }).on('drop', function (el) {
+  }).on('drop', function (el, container, handle) {
+    updateContainerPipeData(container);
+    updateContainerPipeData(handle);
+
     el.className += ' ex-moved';
   }).on('over', function (el, container) {
     container.className += ' ex-over';
@@ -21,7 +82,7 @@ function initEditors() {
 }
 
 function generateId(pipeData) {
-  return pipeData.toLocaleLowerCase();
+  return 'pipe_' + pipeData;
 }
 
 function changeItemHeight(maxLength) {
@@ -46,49 +107,14 @@ function changeItemHeight(maxLength) {
 
 function initElements() {
   var ids = [];
-  var pipeData = [
-    {
-      id: 1,
-      title: 'Process',
-      items: [
-        '提交代码', 'PUSH Hooks', '运行持续集成', '部署到 Dev 环境', 'E2E 测试', '手动测试', '部署到 UAT 环境', '手工测试', '上线申请', '上线'
-      ],
-      backgroundColor: '#00a300',
-      textColor: '#ffffff'
-    },
-    {
-      id: 2,
-      title: 'People',
-      items: [
-        ''
-      ],
-      backgroundColor: '#ff0097',
-      textColor: '#ffffff'
-    },
-    {
-      id: 3,
-      title: 'Tooling',
-      items: [
-        'Git & GitHub', 'Git', 'Jenkins', '', '', '', '', '', '', '', '', ''
-      ],
-      backgroundColor: '#99b433',
-      textColor: '#ffffff'
-    },
-    {
-      id: 4,
-      title: 'Artifacts',
-      items: [
-        'Code', '', 'Build Log', '', '', '', '', '', '', '', '', ''
-      ],
-      backgroundColor: '#1e7145',
-      textColor: '#ffffff'
-    }
-  ];
-
+  var items = localStorage.getItem('ptop.pipe');
+  if (items) {
+    pipeData = JSON.parse(items);
+  }
   var maxLength = pipeData[0].items.length;
   var elements = '';
   for (var i = 0; i < pipeData.length; i++) {
-    var id = generateId(pipeData[i].title);
+    var id = generateId(pipeData[i].id);
     var childItemId = id + '_child';
     var currentHtml = `<div id='${id}' class='container' style='background:${pipeData[i].backgroundColor};color:${pipeData[i].textColor}'>`;
 
@@ -109,7 +135,7 @@ function initElements() {
   changeItemHeight(maxLength);
 
   for (var j = 0; j < pipeData.length; j++) {
-    ids.push(document.getElementById(generateId(pipeData[j].title)));
+    ids.push(document.getElementById(generateId(pipeData[j].id)));
   }
   return ids;
 }
